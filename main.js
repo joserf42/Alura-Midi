e3473cf98860b826102388b4a37f4d50acbeab78
joserf42 <107516670+joserf42@users.noreply.github.com>
2024-08-13 21:19:09 -0500

    Trabajando en main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-function playSonidoPom () {
-    document.querySelector("#sonido_tecla_pom").play();
+function playSonido (idElemtoAudio) {
+    document.querySelector(idElemtoAudio).play();
 }
 
-document.querySelector(".tecla_pom");
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+
+
+let contador = 0
+
+while (contador < 9 ) {
+    // Agregando eventos de teclas
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+    
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    };
+    contador = contador + 1
+   
+}
