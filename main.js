af1a2f6d6235f860c377ed94f9c74b6ce91dba35
joserf42 <107516670+joserf42@users.noreply.github.com>
2024-08-17 16:05:05 -0500

    Optimizando código

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 function playSonido (idElemtoAudio) {
     document.querySelector(idElemtoAudio).play();
 }
-
+// llama a la constante listaDeTeclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
 
 let contador = 0
 
-while (contador < 9 ) {
-    // Agregando eventos de teclas
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    // Creando constantes para los eventos de teclas
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-    
     const idAudio = `#sonido_${instrumento}`;
     console.log(idAudio)
     
+    // Creando evento de click para cada tecla
     tecla.onclick = function () {
         playSonido(idAudio);
     };
-    contador = contador + 1
-   
+    
 }
